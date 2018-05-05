2b4b3c396ec1a45f35f8bb99343451ac8ebe8489
Jan Bucher <jan@jan-bucher.ch>
2018-05-05 13:07:25 +0200

    Add statistics of the sort algorithms
    Statistics where taken on a surface book and run via IntelliJ JUnit Runner.

--- src/main/java/ch/hslu/ad/sw09/package-info.java
+++ src/main/java/ch/hslu/ad/sw09/package-info.java
@@ -1,4 +1,9 @@
 /**
  * This package contains classes for solving the exercises of week 9.
+ *
+ * Comparision of the sort algorithms on a random array (1000 integers - test_sort_randomArray_large()):
+ *  - BubbleSort: 9.396 ms
+ *  - InsertionSort: 10.00 ms
+ *  - SelectionSort: 5.946 ms
  */
 package ch.hslu.ad.sw09;
